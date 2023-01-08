3ce6b31bd95852a3348835cd0536c1bba3945988
john <you@example.com>
2023-01-08 17:13:27 -0600

    fix function errors

--- src/torrent-quick-search.js
+++ src/torrent-quick-search.js
@@ -3,7 +3,7 @@
 // @namespace  https://github.com/TMD20/torrent-quick-search
 // @supportURL https://github.com/TMD20/torrent-quick-search
 // @downloadURL https://greasyfork.org/en/scripts/452502-torrent-quick-search
-// @version     2.02
+// @version     2.03
 // @description Toggle for Searching Torrents via Search aggegrator
 // @icon        https://cdn2.iconfinder.com/data/icons/flat-icons-19/512/Eye.png
 // @author      tmd
@@ -50,9 +50,9 @@
 function main() {
   if (GM.info.script.name == "Torrent Quick Search") {
     overideBuiltins();
-    initConfig();
+    initMainConfig();
     initFilterConfig()
-    createDOM();
+    createMainDOM();
   }
 }
 
