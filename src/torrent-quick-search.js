51ef6e297bd9510fd21ff3b1f14e868550811f00
john <you@example.com>
2023-01-08 13:43:54 -0600

    update version number

--- src/torrent-quick-search.js
+++ src/torrent-quick-search.js
@@ -3,7 +3,7 @@
 // @namespace  https://github.com/TMD20/torrent-quick-search
 // @supportURL https://github.com/TMD20/torrent-quick-search
 // @downloadURL https://greasyfork.org/en/scripts/452502-torrent-quick-search
-// @version     1.70
+// @version     2.00
 // @description Toggle for Searching Torrents via Search aggegrator
 // @icon        https://cdn2.iconfinder.com/data/icons/flat-icons-19/512/Eye.png
 // @author      tmd
